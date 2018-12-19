6003f9357dc19c961db2e2e87fea36bca808a01d
chhhyoung <chhh0118@gmail.com>
2018-12-20 00:46:37 +0900

    aniModifi
    애니메이션 파일 슈정

--- scriptIndex.js
+++ scriptIndex.js
@@ -1,34 +1,41 @@
 var logo = document.getElementById("logo");
-var animation = bodymovin.loadAnimation({
+var connect = bodymovin.loadAnimation({
     container: logo, // Required
-    path: 'data.json', // Required
+    path: 'dataAll.json', // Required
     renderer: 'svg', // Required
     loop: false, // Optional
-    autoplay: true, // Optional
+    autoplay: false, // Optional
     name: "logo", // Name for future reference. Optional.
-  })
-
-var go = document.getElementById("logo");
-var animationGo = bodymovin.loadAnimation({
-    container: logo, // Required
-    path: 'dataAni.json', // Required
-    renderer: 'svg', // Required
-    loop: false, // Optional
-    autoplay: true, // Optional
-    name: "go", // Name for future reference. Optional.
+    rendererSettings:{
+      hideOnTransparent: true
+    }
 })
 
+// var connecting = bodymovin.loadAnimation({
+//     container: logo, // Required
+//     path: 'dataAni.json', // Required
+//     renderer: 'svg', // Required
+//     loop: false, // Optional
+//     autoplay: true, // Optional
+//     name: "logoAni", // Name for future reference. Optional.
+//     rendererSettings:{
+//       hideOnTransparent: true
+//     }
+// })
+
 logo.addEventListener("mouseenter", function () {
-animation.play();
+connect.playSegments([0, 31], true);
 });
 
 logo.addEventListener("mouseleave", function () {
-animation.stop();
+connect.stop();
 });
 
-go.addEventListener("click", function () {
-  animation.play();
-  });
+
+logo.addEventListener("click", function () {
+  connect.playSegments([33, 100], true);
+  
+});
 
 // var animation = bodymovin.loadAnimation({
 //     container: document.getElementById('bm'),
@@ -36,4 +43,4 @@ go.addEventListener("click", function () {
 //     loop: true,
 //     autoplay: true,
 //     path: 'dataLine.json'
-//   })
+//   })connecting.hideOnTransparent();
